91ad5ec59a04adb4c57e0134781d5b23aa6bf430
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2022-01-25 09:55:54 -0400

    setup fetch and inital display of gas fees

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,70 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+      <div class="gas-stats">
+        <div class="speed">
+          <div>
+            <span>SafeLow</span>
+          </div>
+          <div>
+            <span>Max Priority Fee: {{this.safeLow.maxPriorityFee}}</span>
+          </div>
+          <div>
+            <span>Max Fee: {{this.safeLow.maxFee}}</span>
+          </div>
+        </div>
+        <div class="speed">
+          <div>
+            <span>Standard</span>
+          </div>
+          <div>
+            <span>Max Priority Fee: {{this.standard.maxPriorityFee}}</span>
+          </div>
+          <div>
+            <span>Max Fee: {{this.standard.maxFee}}</span>
+          </div>
+        </div>
+        <div class="speed">
+          <div>
+            <span>Fast</span>
+          </div>
+          <div>
+            <span>Max Priority Fee: {{this.fast.maxPriorityFee}}</span>
+          </div>
+          <div>
+            <span>Max Fee: {{this.fast.maxFee}}</span>
+          </div>
+        </div>
+      </div>
+    <h2> Estimated Base Fee: {{this.currentGas.estimatedBaseFee}}</h2>
+    <h2>Block Time: {{this.currentGas.blockTime}}</h2>
+    <h2>Block number: {{this.currentGas.blockNumber}}</h2>
   </div>
 </template>
 
 <script>
+import * as polygon from "./ploygonFetch.js"
 export default {
   name: 'HelloWorld',
+  data(){
+    return{
+      currentGas:null,
+      safeLow:null,
+      standard:null,
+      fast:null,
+      count:0,
+    }
+  },
   props: {
     msg: String
+  },
+
+  async created(){
+    this.currentGas = await polygon.fetch();
+    this.safeLow=this.currentGas.safeLow;
+    this.standard = this.currentGas.standard;
+    this.fast= this.currentGas.fast;
   }
 }
 </script>
@@ -55,4 +85,15 @@ li {
 a {
   color: #42b983;
 }
+.gas-stats::before {
+        content: "";
+        margin: 10px;
+}
+.speed{
+  flex-grow: 8;
+  text-align: center;
+  padding-left: 20px;
+  font-size: 25px;
+}
+
 </style>
